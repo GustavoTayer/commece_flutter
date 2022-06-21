2cead5dfaf0796d9148b4d87f418c077740a737e
Tayer <tayergustavo@hotmail.com>
2022-06-20 21:00:53 -0300

    comentando código quebrado

--- lib/main.dart
+++ lib/main.dart
@@ -24,147 +24,111 @@ void main() async {
 
 }
 
-class Controller extends GetxController {
-  var count = 0.obs;
-
-  increment() => count++;
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(context) {
-    // Instantiate your class using Get.put() to make it available for all "child" routes there.
-    final Controller c = Get.put(Controller());
-
-    return Scaffold(
-      // Use Obx(()=> to update Text() whenever count is changed.
-      appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
-      actions: [
-      IconButton(
-        onPressed: () {
-          ShowSearch(
-            context: context,
-            delegate: CustomSearchDelegate(),);
-        },
-          icon: const Icon(Icons.Search),
-      ),
-      ],
-      // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Go to Other"),
-          onPressed: () => Get.to(() => Other(),
-              transition: Transition.upToDown,
-              duration: const Duration(milliseconds: 200)),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: c.increment,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class CustomSearchDelegate extends SearchDelegate {
-List<String> searchTerms = [
-  'mochila',
-  'camiseta',
-];
-
-@override
-List<widget> buildActions(BuildContext context) {
-  return [
-    IconButton(
-      icon: const Icon(Icons.clear),
-      onPressed: (){
-        query = '';
-      },
-    ),
-  ];
-}
-
-@override
-widget buildLeading(BuildContext context) {
-  return IconButton(
-    icon: const Icon(Icons.arrow_back),
-    onPressed: () {
-      close(context, null);
-    },
-  );
-}
-
-@override
-widget buildResults(BuildContext context) {
-  list<String> matchQuery = [];
-  for (var roupas in searchTerms) {
-    if (roupas,toLowerCase().contains(query.toLowerCase())) {
-      matchQuery.add(roupas);
-    }
-  }
-  return ListView.builder(
-    itemCount: matchQuery.length, 
-    itemBuilder: (context, index) {
-      var result = matchQuery[index];
-      return ListTile(
-        title: Text(result),
-      );
-    },
-  );
-}
-
-@override
-widget buildSuggestions(BuildContext context) {
-  List<String> matchQuery = [];
-  for (var roupas in searchTerms) {
-    if (roupas.toLowerCase().contains(query.toLowerCase())) {
-      matchQuery.add(roupas);
-    }
-  }
-  return ListView.builder(
-    itemCount: matchQuery.length,
-    itemBuilder: (context, index) {
-      var result = matchQuery[index];
-      return ListTile(
-        title: Text(result),
-      );
-    },
-  );
-  
-}
-
-}
-
-class Other extends StatelessWidget {
-  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
-  final Controller c = Get.find();
-
-  @override
-  Widget build(context) {
-    // Access the updated count variable
-    return Scaffold(
-      body: Center(
-        child: Card(
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            shrinkWrap: true,
-            padding: const EdgeInsets.all(16),
-            children: [
-              Center(child: Text("${c.count}")),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 36.0, vertical: 10),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Get.back();
-                  },
-                  child: const Text("Voltar"),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class Controller extends GetxController {
+//   var count = 0.obs;
+//
+//   increment() => count++;
+// }
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(context) {
+//     // Instantiate your class using Get.put() to make it available for all "child" routes there.
+//     final Controller c = Get.put(Controller());
+//
+//     return Scaffold(
+//       // Use Obx(()=> to update Text() whenever count is changed.
+//       appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+//       actions: [
+//       IconButton(
+//         onPressed: () {
+//           ShowSearch(
+//             context: context,
+//             delegate: CustomSearchDelegate(),);
+//         },
+//           icon: const Icon(Icons.Search),
+//       ),
+//       ],
+//       // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
+//       body: Center(
+//         child: ElevatedButton(
+//           child: const Text("Go to Other"),
+//           onPressed: () => Get.to(() => Other(),
+//               transition: Transition.upToDown,
+//               duration: const Duration(milliseconds: 200)),
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: c.increment,
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+//
+// class CustomSearchDelegate extends SearchDelegate {
+// List<String> searchTerms = [
+//   'mochila',
+//   'camiseta',
+// ];
+//
+// @override
+// List<widget> buildActions(BuildContext context) {
+//   return [
+//     IconButton(
+//       icon: const Icon(Icons.clear),
+//       onPressed: (){
+//         query = '';
+//       },
+//     ),
+//   ];
+// }
+//
+// @override
+// widget buildLeading(BuildContext context) {
+//   return IconButton(
+//     icon: const Icon(Icons.arrow_back),
+//     onPressed: () {
+//       close(context, null);
+//     },
+//   );
+// }
+//
+// @override
+// widget buildResults(BuildContext context) {
+//   list<String> matchQuery = [];
+//   for (var roupas in searchTerms) {
+//     if (roupas,toLowerCase().contains(query.toLowerCase())) {
+//       matchQuery.add(roupas);
+//     }
+//   }
+//   return ListView.builder(
+//     itemCount: matchQuery.length,
+//     itemBuilder: (context, index) {
+//       var result = matchQuery[index];
+//       return ListTile(
+//         title: Text(result),
+//       );
+//     },
+//   );
+// }
+//
+// @override
+// widget buildSuggestions(BuildContext context) {
+//   List<String> matchQuery = [];
+//   for (var roupas in searchTerms) {
+//     if (roupas.toLowerCase().contains(query.toLowerCase())) {
+//       matchQuery.add(roupas);
+//     }
+//   }
+//   return ListView.builder(
+//     itemCount: matchQuery.length,
+//     itemBuilder: (context, index) {
+//       var result = matchQuery[index];
+//       return ListTile(
+//         title: Text(result),
+//       );
+//     },
+//   );
+//
+// }
+// }
